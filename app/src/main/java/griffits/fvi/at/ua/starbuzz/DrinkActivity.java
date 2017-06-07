35aa9149b160226d504f1711e53b709a90b235f3
Victoria Frantsuh <eboko1@gmail.com>
2017-06-07 16:28:23 +0300

    add photo ,name  in DrinkActivity

--- app/src/main/java/griffits/fvi/at/ua/starbuzz/DrinkActivity.java
+++ app/src/main/java/griffits/fvi/at/ua/starbuzz/DrinkActivity.java
@@ -2,10 +2,13 @@ package griffits.fvi.at.ua.starbuzz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrinkActivity extends AppCompatActivity {
 
-
+     TextView name, description;
+     ImageView photo;
     public static final String EXTRA_DRINKNO = "drinkNo" ;
 
     @Override
@@ -13,8 +16,21 @@ public class DrinkActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
 
-        int  drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
+        init();
 
+        int  drinkNo = (Integer)getIntent().getExtras().get(EXTRA_DRINKNO);
+        Drink drink = Drink.drinks[drinkNo];
+
+        //add photo
+        //photo.setImageResource(drink.getImgResId());
+      //  photo.setContentDescription(drink.getName());
         
+
+    }
+
+    public void init(){
+        ImageView photo = (ImageView)findViewById(R.id.photo);
+        TextView name = (TextView)findViewById(R.id.name);
+        TextView description = (TextView)findViewById(R.id.description);
     }
 }
