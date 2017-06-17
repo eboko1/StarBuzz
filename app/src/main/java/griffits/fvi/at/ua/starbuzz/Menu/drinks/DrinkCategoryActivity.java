4bc29a7cc5b90e39da74d47c1312570c6366ecf6
Victoria Frantsuh <eboko1@gmail.com>
2017-06-17 09:10:04 +0300

    MakeChangesMenuActivity

--- app/src/main/java/griffits/fvi/at/ua/starbuzz/Menu/drinks/DrinkCategoryActivity.java
+++ app/src/main/java/griffits/fvi/at/ua/starbuzz/Menu/drinks/DrinkCategoryActivity.java
@@ -4,7 +4,6 @@ import android.app.ListActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
--- app/src/main/java/griffits/fvi/at/ua/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/griffits/fvi/at/ua/starbuzz/StarbuzzDatabaseHelper.java
@@ -20,6 +20,12 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "Starbuzz";
     private static final int DB_VERSION = 1;  // for update database change  version  --->2
 
+    private static final String TABLE_CREATE_DRINK = "CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + "NAME TEXT, "
+            + "DESCRIPTION TEXT, "
+            + "CATEGORY TEXT, "
+            + "IMAGE_RESOURCE_ID INTEGER);";
+
      public StarbuzzDatabaseHelper(Context context){
       super(context,DB_NAME, null, DB_VERSION);
         Log.i(LOG_INFO, "StarbuzzDatabaseHelper constructor ");
@@ -42,14 +48,10 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion){
         if (oldVersion < 1) {
-            db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                    + "NAME TEXT, "
-                    + "DESCRIPTION TEXT, "
-                    + "IMAGE_RESOURCE_ID INTEGER);");
-            insertDrink(db, "Latte", "Espresso and steamed milk", R.mipmap.ic_latte);
-            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
-                    R.mipmap.ic_cappuccino);
-            insertDrink(db, "Filter", "Our best drip coffee", R.mipmap.ic_filter);
+            db.execSQL(TABLE_CREATE_DRINK);
+            insertDrink(db, "Latte", "Espresso and steamed milk", "drink" , R.mipmap.ic_latte);
+            insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam", "drink", R.mipmap.ic_cappuccino);
+            insertDrink(db, "Filter", "Our best drip coffee", "drink", R.mipmap.ic_filter);
         }
         if (oldVersion < 2) {
             db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
