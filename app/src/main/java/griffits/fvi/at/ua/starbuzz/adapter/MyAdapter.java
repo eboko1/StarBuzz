2e8ab28ff0434fda6be966ea8b83fdb204c8b1f7
Victoria Frantsuh <eboko1@gmail.com>
2017-06-19 20:31:30 +0300

    add items_list

--- app/src/main/java/griffits/fvi/at/ua/starbuzz/adapter/MyAdapter.java
+++ app/src/main/java/griffits/fvi/at/ua/starbuzz/adapter/MyAdapter.java
@@ -1,27 +1,41 @@
 package griffits.fvi.at.ua.starbuzz.adapter;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 /**
  * Created by Vika on 19.06.2017.
  */
 
 public class MyAdapter extends BaseAdapter {
+    Context context;
+    private ArrayList<Integer> listId;
+    private ArrayList<String> nameList;
+
+    public MyAdapter(Context context, ArrayList<Integer> listId, ArrayList<String> nameList) {
+        this.context = context;
+        this.listId = listId;
+        this.nameList = nameList;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return nameList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return nameList.get(position);
     }
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
     }
 
     @Override
