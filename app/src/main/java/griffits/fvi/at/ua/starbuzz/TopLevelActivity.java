d1148f6fe892eb8201083abd2657d2d4ba21a4c1
Victoria Frantsuh <eboko1@gmail.com>
2017-06-19 20:24:27 +0300

    add ArrayList for image

--- app/src/main/java/griffits/fvi/at/ua/starbuzz/TopLevelActivity.java
+++ app/src/main/java/griffits/fvi/at/ua/starbuzz/TopLevelActivity.java
@@ -1,80 +1,75 @@
 package griffits.fvi.at.ua.starbuzz;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.ConditionVariable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import griffits.fvi.at.ua.starbuzz.Menu.dessert.DessertCategoryActivity;
 import griffits.fvi.at.ua.starbuzz.Menu.drinks.DrinkCategoryActivity;
 
 public class TopLevelActivity extends AppCompatActivity {
 
-    private ListView listViewOption;
 
-    int [] imagesMenu = {R.drawable.drink,
-                            R.drawable.breakfast,
-                            R.drawable.sandwich,
-                            R.drawable.burger,
-                            R.drawable.pizza2,
-                            R.drawable.dessert,
-                            R.drawable.ic_adding};
 
-    String[] titlesMenu = { "Drinks",
-                            "Breakfast",
-                            "Sandwiches",
-                            "Burgers",
-                            "Pizzas",
-                            "Desserts",
-                            "Add in menu"};
+    private static final String LOG_INFO = "mylog";
+
+    private Context context;
+    private ArrayList<Integer> listId;
+    private ArrayList<String> listTitle;
+
+    ListView listViewOption;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top_level);
+        Log.i(LOG_INFO, "onCreate in TopLevelActivity");
 
-        init();
+        listViewOption = (ListView)findViewById(R.id.list_options);
 
-
-        AdapterView.OnItemClickListener itemClList = new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-             if(position == 0){
-                 Toast.makeText(getApplication(),"Drinks", Toast.LENGTH_SHORT).show();
-                 startActivity(new Intent(getApplicationContext(), DrinkCategoryActivity.class));
-             } else if(position == 1){
-                  Toast.makeText(getApplication(),"Breakfast", Toast.LENGTH_SHORT).show();
-             }else if(position == 2){
-                 Toast.makeText(getApplication(),"Sandwiches", Toast.LENGTH_SHORT).show();
-             } else if(position == 3){
-                 Toast.makeText(getApplication(),"Burgers", Toast.LENGTH_SHORT).show();
-             } else if(position == 4){
-                 Toast.makeText(getApplication(),"Pizzas", Toast.LENGTH_SHORT).show();
-             } else if(position == 5) {
-                 Toast.makeText(getApplication(), "Desserts", Toast.LENGTH_SHORT).show();
-                 startActivity(new Intent(getApplicationContext(), DessertCategoryActivity.class));
-             } else if(position == 6){
-                 Toast.makeText(getApplication(),"Specials", Toast.LENGTH_SHORT).show();
-             } else if(position == 7){
-                 Toast.makeText(getApplication(),"Add in menu", Toast.LENGTH_SHORT).show();
-                 onClickAddToMenu();
-             }
-            }
-        };
-
-        listViewOption.setOnItemClickListener(itemClList);
 
 
     }
 
-    public void  init(){
-        listViewOption  = (ListView)findViewById(R.id.list_options);
+    public ArrayList<Integer> getListImageId(){
+        listId = new ArrayList<Integer>();
+        listId.add(R.drawable.drink);
+        listId.add(R.drawable.breakfast);
+        listId.add(R.drawable.sandwich);
+        listId.add(R.drawable.burger);
+        listId.add(R.drawable.pizza2);
+        listId.add(R.drawable.dessert);
+        listId.add(R.drawable.ic_adding);
+
+        return listId;
     }
 
-    public void onClickAddToMenu(){
-        startActivity(new Intent(this, MakeChangesMenuActivity.class));
+    public ArrayList<String> getListTitleL(){
+        listTitle = new ArrayList<String>();
+        listTitle.add("Drinks");
+        listTitle.add("Breakfast");
+        listTitle.add("Sandwiches");
+        listTitle.add("Burgers");
+        listTitle.add("Pizzas");
+        listTitle.add("Desserts");
+        listTitle.add("Add in menu");
+
+        return listTitle;
     }
+
+
 
 }
