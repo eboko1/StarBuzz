a737625d11215a126174ec152c33a55e035b545a
Victoria Frantsuh <eboko1@gmail.com>
2017-06-22 21:07:04 +0300

    adding openGallery()

--- app/src/main/java/griffits/fvi/at/ua/starbuzz/MakeChangesMenuActivity.java
+++ app/src/main/java/griffits/fvi/at/ua/starbuzz/MakeChangesMenuActivity.java
@@ -2,10 +2,13 @@ package griffits.fvi.at.ua.starbuzz;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.graphics.Canvas;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +21,8 @@ import android.widget.Toast;
 
 public class MakeChangesMenuActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String LOG_INFO = "mylog";
+    private static final int PICK_NAME = 100;
+    Uri imageUri;
 
     private EditText et_name, et_description, et_category;
     private Button btn_add_data_table, btn_delete_data_table, btn_clean_data_table, btn_show_data_table, btn_load_image ;
@@ -61,8 +66,11 @@ public class MakeChangesMenuActivity extends AppCompatActivity implements View.O
         btn_delete_data_table = (Button)findViewById(R.id.btn_delete_datatable);
         btn_clean_data_table = (Button)findViewById(R.id.btn_clean_datatable);
         btn_show_data_table = (Button)findViewById(R.id.btn_show_datatable);
-        btn_load_image = (Button)findViewById(R.id.btn_load_image);
         screen = (TextView)findViewById(R.id.tv_screen);
+
+        btn_load_image = (Button)findViewById(R.id.btn_load_image);
+        image_load = (ImageView)findViewById(R.id.image_load);
+
     }
 
     @Override
@@ -89,11 +97,24 @@ public class MakeChangesMenuActivity extends AppCompatActivity implements View.O
                 starbuzzDatabaseHelper.list_all_table(screen);
                 break;
             case R.id.btn_load_image:
-
-
+                openGallery();
                 break;
         }
 
-
     }
+
+    private void openGallery(){
+        Intent intentGallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(intentGallery, PICK_NAME );
+    }
+
+    @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == RESULT_OK && requestCode == PICK_NAME){
+                imageUri = data.getData();
+                image_load.setImageURI(imageUri);
+        }
+    }
+
 }
